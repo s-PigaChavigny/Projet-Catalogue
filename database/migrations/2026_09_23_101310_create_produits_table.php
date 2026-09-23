262cdfed3899d0a847b61b3a90e34ddc2e459b78
Sibel-Si <sibel.sever@mmibordeaux.com>
2026-09-23 16:20:52 +0200

    modification prix fonction produit

--- database/migrations/2026_09_23_101310_create_produits_table.php
+++ database/migrations/2026_09_23_101310_create_produits_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('image_path')->nullable();
+            $table->decimal("price", total : 10, places : 2);
         });
     }
 
