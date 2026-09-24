b60571982b1fa5793b9a8d14842d63c223ab3a15
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-24 10:19:22 +0200

    final fix css

--- resources/views/boutiques/all.blade.php
+++ resources/views/boutiques/all.blade.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mes boutiques</title>
-    <link rel="stylesheet" href="{{ asset('css/boutique.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 </head>
 <body>
     @include('partials.header')
