3fbca1e01a3004924a02b12747312694f7c90a18
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-24 10:17:34 +0200

    fix clean up css

--- resources/views/boutiques/show.blade.php
+++ resources/views/boutiques/show.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $boutique->name }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="page">
