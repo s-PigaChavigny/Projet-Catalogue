5b85018230f9f2ae40a8b863f378333a14d88b18
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-24 15:47:12 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -179,4 +179,3 @@ Route::get('/admin', function () {
     ]);
 })->middleware(['auth', 'admin'])->name('admin');
 
-Route::get("/search",[SearchController::class, "search.show"])->name("search.show");
