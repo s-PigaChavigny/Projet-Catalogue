982fc6a2a9dc8d8a5b0964d42ae8cbc5de2d018e
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-25 10:20:29 +0200

    css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Happy+Monkey&family=Lemon&display=swap');
+    </style>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <title>Connexion</title>
 </head>
